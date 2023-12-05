c73c901f2ba2abe168c5a0a5179200150cc4b86f
carlos H <carlitobr730@yahoo.com>
2023-12-05 16:10:56 -0400

    bugs retirados v2

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ let img = document.getElementById("img");
 let check = document.getElementById("check");
 let header = document.getElementById("header");
 const pheader = document.getElementById("textoheader");
-const esconder = document.getElementById("esconder");
 let retirarPrint = document.getElementById("print");
 
 const dialog = document.querySelector("dialog"); // Note que as aspas estão corrigidas
@@ -38,18 +37,6 @@ fecharDialog.addEventListener("click", function () {
 
 
 
-function esconderTexto() {
-  const textos = document.getElementById("textos");
-  if (textos.style.display === "none") {
-    textos.style.display = "block";
-  } else {
-    textos.style.display = "none";
-  }
-}
-
-esconder.addEventListener("click", function () {
-  esconderTexto();
-});
 
 //cria uma funcao no scroll para quando rolar ficar trasnaparente.
 
@@ -61,13 +48,9 @@ function media() {
         document.body.scroll > 50 ||
         document.documentElement.scrollTop > 50
       ) {
-        header.style.backgroundColor = "transparent";
         menu.style.color = "black";
-        pheader.style.display = "none";
       } else {
-        header.style.backgroundColor = "white";
         menu.style.color = "black";
-        pheader.style.display = "block";
       }
     };
   }
@@ -87,7 +70,6 @@ function gerarQrcode() {
   let input = document.getElementById(`input`).value;
   const color1 = document.getElementById("color1").value;
   const color2 = document.getElementById("color2").value;
-
   const pequeno = document.getElementById(`pequeno`);
   const grande = document.getElementById(`grande`);
   const medio = document.getElementById(`medio`);
@@ -97,22 +79,24 @@ function gerarQrcode() {
       text: input,
       colorDark: `white`,
       colorLight: color2,
+      width: 150,
+      height: 150,
     });
   } else if (pequeno.checked) {
     qr = new QRCode(document.getElementById("qr"), {
       text: input,
       colorDark: color1,
       colorLight: color2,
-      width: 100,
-      height: 100,
+      width: 200,
+      height: 200,
     });
   } else if (grande.checked) {
     qr = new QRCode(document.getElementById("qr"), {
       text: input,
       colorDark: color1,
       colorLight: color2,
-      width: 400,
-      height: 400,
+      width: 350,
+      height: 350,
     });
   } else if (medio.checked) {
     qr = new QRCode(document.getElementById("qr"), {
@@ -155,6 +139,7 @@ function exibirToastSucesso() {
 }
 
 function capturar() {
+  
   html2canvas(document.getElementById("qr")).then(function(canvas) {
     let dataURL = canvas.toDataURL();
     let link = document.createElement(`a`);
